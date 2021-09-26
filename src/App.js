9916d6cd941224456d22bb317839294223162f75
marcefalb <marcefalb@gmail.com>
2021-09-26 15:32:39 +0300

    my first commit:)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/general/Header'
+
+import './assets/styles/App.css'
+
+const Main = lazy(() => import('./components/pages/Main'))
+const Repo = lazy(() => import('./components/pages/Repo'))
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header/>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Switch>
+          <Route exact path='/' component={Main}/>
+          <Route path='/Repo/:owner/:name' component={Repo}/>
+        </Switch>
+      </Suspense>
+    </Router>
+  )
 }
 
-export default App;
+export default App
