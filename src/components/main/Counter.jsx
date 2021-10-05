25e95b5033628b7ca4c64d488c26f9dc83992c0a
marcefalb <marcefalb@gmail.com>
2021-10-05 03:20:47 +0300

    mobx maximum decomposition (probably...)

--- src/components/main/Counter.jsx
+++ src/components/main/Counter.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from 'react'
+import counter from './../../store/counter'
+import search from './../../store/search'
+import {observer} from "mobx-react-lite";
 
-const Counter = ({total_count}) => {
+const Counter = observer(() => {
   return (
     <div className="counter">
       <span className="counter__title">Количество репозиториев:</span>
-      <span className="counter__num">{total_count}</span>
+      <span className="counter__num">{counter.setTotalCount(search.response.total_count)}</span>
     </div>
   )
-};
+})
 
 export default Counter
